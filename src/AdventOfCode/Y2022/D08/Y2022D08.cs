f709662ee7009d82365db97288f63c1f9ce6dc9f
Eric M <itsjusteza@gmail.com>
2022-12-09 00:53:41 +1000

    2022 D08 Refactor

--- src/AdventOfCode/Y2022/D08/Y2022D08.cs
+++ src/AdventOfCode/Y2022/D08/Y2022D08.cs
@@ -3,9 +3,6 @@ using AdventOfCode.Lib.Attributes;
 
 namespace AdventOfCode.Y2022.D08;
 
-/// <summary>
-/// Pretty disgusting
-/// </summary>
 public class Y2022D08 : ISolution
 {
     [Flags]
@@ -20,142 +17,88 @@ public class Y2022D08 : ISolution
 
     private readonly Direction[] _dirArr = {Direction.Left, Direction.Right, Direction.Top, Direction.Bottom};
 
-    [ExpectedResult("", Sample = 21)]
+    [ExpectedResult(1870, Sample = 21)]
     public object SolvePartOne(string input)
     {
-        string[] lines = InputHelpers.AsLines(input)
-                                     .ToArray();
+        int gw = input.IndexOf('\n');
+        int[] trees = InputHelpers.AsLines(input)
+                                  .SelectMany(l => l.Select(c => int.Parse(c.ToString())))
+                                  .ToArray();
 
-        int gw = lines[0].Length;
-
-        var trees = lines.SelectMany(l => l.Select(c => int.Parse(c.ToString())))
-                         .ToArray();
-
-        int visible = 0;
-        Direction directionsBlocked = 0;
-
-        for (int i = 0; i < trees.Length; i++)
-        {
-            if (i % gw == 0 || i < gw || i % gw == gw - 1 || i > (gw - 1) * gw)
-            {
-                // Borders/perimeter
-                visible++;
-                continue;
-            }
-
-            int checkRadius = 1;
-            bool done = false;
-            while (!done)
-            {
-                foreach (var dir in _dirArr.Where(d => (directionsBlocked & d) != d))
-                {
-                    var height = dir switch
-                    {
-                        Direction.Left => i / gw != (i - checkRadius) / gw ? -1 : trees[i - checkRadius],
-                        Direction.Right => i / gw != (i + checkRadius) / gw ? -1 : trees[i + checkRadius],
-                        Direction.Top => i - checkRadius * gw < 0 ? -1 : trees[i - checkRadius * gw],
-                        Direction.Bottom => i + checkRadius * gw > trees.Length ? -1 : trees[i + checkRadius * gw],
-                        _ => throw new ArgumentOutOfRangeException()
-                    };
-
-                    if (height == -1 && (directionsBlocked &= dir) != dir)
-                    {
-                        visible++;
-                        done = true;
-                        break;
-                    }
-
-                    if (height >= trees[i])
-                    {
-                        directionsBlocked |= dir;
-                    }
-
-                    if (directionsBlocked == Direction.All)
-                    {
-                        done = true;
-                        break;
-                    }
-                }
-
-                checkRadius++;
-            }
-
-            directionsBlocked = 0;
-        }
-
-
-        return visible;
+        return trees.Select((_, i) => GetTreeStats(trees, i, gw))
+             .Count(s => s.visibleFromEdge);
     }
 
-    [ExpectedResult("", Sample = 8)]
+    [ExpectedResult(517440, Sample = 8)]
     public object SolvePartTwo(string input)
     {
-        // Flatten
-        string[] lines = InputHelpers.AsLines(input)
-                                     .ToArray();
+        int gw = input.IndexOf('\n');
+        int[] trees = InputHelpers.AsLines(input)
+                                  .SelectMany(l => l.Select(c => int.Parse(c.ToString())))
+                                  .ToArray();
 
-        int gw = lines[0].Length;
+        return trees.Select((_, i) => GetTreeStats(trees, i, gw))
+                    .Max(s => s.score);
+    }
 
-        var trees = lines.SelectMany(l => l.Select(c => int.Parse(c.ToString())))
-                         .ToArray();
-
-        int bestScore = 0;
+    private (bool visibleFromEdge, int score) GetTreeStats(int[] trees, int i, int gw)
+    {
+        bool visible = false;
         Dictionary<Direction, int> currScores = _dirArr.ToDictionary(dir => dir, _ => 0);
+
+        bool done = false;
+        int checkRadius = 1;
         Direction directionsBlocked = 0;
 
-        for (int i = 0; i < trees.Length; i++)
+        while (!done)
         {
-            int checkRadius = 1;
-            bool done = false;
-            while (!done)
+            foreach (var dir in _dirArr.Where(d => (directionsBlocked & d) != d))
             {
-                foreach (var dir in _dirArr.Where(d => (directionsBlocked & d) != d))
+                // Return -1 as the height if:
+                // * The location to check is out of bounds of the array
+                // * Going left/right would put you into a different row of trees
+                int height = dir switch
                 {
-                    int height = dir switch
-                    {
-                        Direction.Left => (i - checkRadius) < 0 || i / gw != (i - checkRadius) / gw ? -1 : trees[i - checkRadius],
-                        Direction.Right => (i + checkRadius) < 0 || i / gw != (i + checkRadius) / gw ? -1 : trees[i + checkRadius],
-                        Direction.Top => (i - checkRadius * gw) > trees.Length - 1 || i - checkRadius * gw < 0 ? -1 : trees[i - checkRadius * gw],
-                        Direction.Bottom => (i + checkRadius * gw) > trees.Length - 1 || i + checkRadius * gw > trees.Length ? -1 : trees[i + checkRadius * gw],
-                        _ => throw new ArgumentOutOfRangeException()
-                    };
+                    Direction.Left => (i - checkRadius) < 0 || i / gw != (i - checkRadius) / gw
+                        ? -1
+                        : trees[i - checkRadius],
+                    Direction.Right => (i + checkRadius) < 0 || i / gw != (i + checkRadius) / gw
+                        ? -1
+                        : trees[i + checkRadius],
+                    Direction.Top => i - checkRadius * gw < 0
+                        ? -1
+                        : trees[i - checkRadius * gw],
+                    Direction.Bottom => (i + checkRadius * gw) > trees.Length - 1 
+                        ? -1
+                        : trees[i + checkRadius * gw],
+                    _ => throw new ArgumentOutOfRangeException()
+                };
 
-                    if (height == -1)
-                    {
-                        directionsBlocked |= dir;
-                    }
-                    else if (height >= trees[i])
-                    {
-                        currScores[dir]++;
-                        directionsBlocked |= dir;
-                    }
-                    else
-                    {
-                        currScores[dir]++;
-                    }
-
-                    if (directionsBlocked == Direction.All)
-                    {
-                        done = true;
-                        break;
-                    }
+                if (height == -1)
+                {
+                    visible = visible || (directionsBlocked & dir) != dir || checkRadius == 1;
+                    directionsBlocked |= dir;
+                }
+                else if (height >= trees[i])
+                {
+                    currScores[dir]++;
+                    directionsBlocked |= dir;
+                }
+                else
+                {
+                    currScores[dir]++;
                 }
 
-                checkRadius++;
+                if (directionsBlocked == Direction.All)
+                {
+                    done = true;
+                    break;
+                }
             }
 
-            var score = currScores.Values.Aggregate((tot, v) => tot * v);
-            if (score > bestScore)
-            {
-                Console.WriteLine($"new best tree {i}: {score}");
-                bestScore = score;
-            }
-            
-            currScores = _dirArr.ToDictionary(dir => dir, _ => 0);
-            directionsBlocked = 0;
+            checkRadius++;
         }
 
-
-        return bestScore;
+        return (visible, currScores.Values.Aggregate((tot, v) => tot * v));
     }
 }
