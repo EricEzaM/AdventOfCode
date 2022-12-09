c7cf9f7d9b3bdd15b6faa25164f23898ea6f684c
Eric M <itsjusteza@gmail.com>
2022-12-09 21:19:24 +1000

    2022 D09 Complete

--- src/AdventOfCode/Y2022/D09/Y2022D09.cs
+++ src/AdventOfCode/Y2022/D09/Y2022D09.cs
@@ -8,9 +8,10 @@ public class Y2022D09 : ISolution
     private record struct Pos(int X, int Y)
     {
         public static Pos operator +(Pos a, Pos b) => new Pos(a.X + b.X, a.Y + b.Y);
+        public static Pos operator -(Pos a, Pos b) => new Pos(a.X - b.X, a.Y - b.Y);
     };
 
-    
+
     [ExpectedResult(6498, Sample = 13)]
     public object SolvePartOne(string input)
     {
@@ -18,41 +19,66 @@ public class Y2022D09 : ISolution
                                     .Select(l => (l[..1], int.Parse(l[2..])))
                                     .ToArray();
 
+        return GetUniquePositions(movements, 2, 1);
+    }
+
+    /// <summary>
+    /// Note for Part 2 there is a separate more complex sample in addition to the first (smaller) sample.
+    /// </summary>
+    [ExpectedResult(2531, Sample = "")]
+    public object SolvePartTwo(string input)
+    {
+        var movements = InputHelpers.AsLines(input)
+                                    .Select(l => (l[..1], int.Parse(l[2..])))
+                                    .ToArray();
+
+        return GetUniquePositions(movements, 10, 9);
+    }
+
+    private int GetUniquePositions((string, int)[] headMovements, int numPoints, int trackPointIdx)
+    {
         HashSet<Pos> uniquePos = new();
 
-        var h = new Pos();
-        var t = new Pos();
-        
-        foreach ((string dir, int dist) in movements)
+        var points = new Pos[numPoints];
+
+        foreach ((string dir, int dist) in headMovements)
         {
             for (int step = 0; step < dist; step++)
             {
-                t += dir switch
-                {
-                    "L" => h.X < t.X ? new Pos(-1, h.Y - t.Y) : new Pos(),
-                    "R" => h.X > t.X ? new Pos(1, h.Y - t.Y) : new Pos(),
-                    "D" => h.Y < t.Y ? new Pos(h.X - t.X, -1) : new Pos(),
-                    "U" => h.Y > t.Y ? new Pos(h.X - t.X, 1) : new Pos(),
-                };
-                
-                h += dir switch
+                points[0] += dir switch
                 {
                     "L" => new Pos(-1, 0),
                     "R" => new Pos(1, 0),
                     "D" => new Pos(0, -1),
                     "U" => new Pos(0, 1),
                 };
-                
-                uniquePos.Add(t);
+
+                for (int pi = 1; pi < points.Length; pi++)
+                {
+                    var h = points[pi - 1];
+                    var t = points[pi];
+                    var delta = h - t;
+                    points[pi] += delta switch
+                    {
+                        {X: > 1, Y: > 1} => delta + new Pos(-1, -1),
+                        {X: < -1, Y: < -1} => delta + new Pos(1, 1),
+                        {X: < -1, Y: > 1} => delta + new Pos(1, -1),
+                        {X: > 1, Y: < -1} => delta + new Pos(-1, 1),
+                        {X: > 1} => delta with {X = 1},
+                        {X: < -1} => delta with {X = -1},
+                        {Y: > 1} => delta with {Y = 1},
+                        {Y: < -1} => delta with {Y = -1},
+                        _ => new Pos()
+                    };
+
+                    if (pi == trackPointIdx)
+                    {
+                        uniquePos.Add(points[pi]);
+                    }
+                }
             }
         }
 
         return uniquePos.Count;
-    }
-
-    [ExpectedResult("", Sample = "")]
-    public object SolvePartTwo(string input)
-    {
-        return string.Empty;
     }
 }
