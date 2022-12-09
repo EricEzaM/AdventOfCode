8be982167d99c154ac56dacc0749308b2df06fbf
Eric M <itsjusteza@gmail.com>
2022-12-09 23:38:58 +1000

    Speed improvements to Y2022 D09

--- src/AdventOfCode/Y2022/D09/Y2022D09.cs
+++ src/AdventOfCode/Y2022/D09/Y2022D09.cs
@@ -7,10 +7,23 @@ public class Y2022D09 : ISolution
 {
     private record struct Pos(int X, int Y)
     {
-        public static Pos operator +(Pos a, Pos b) => new Pos(a.X + b.X, a.Y + b.Y);
-        public static Pos operator -(Pos a, Pos b) => new Pos(a.X - b.X, a.Y - b.Y);
+        public static Pos operator +(Pos a, Pos b) => new(a.X + b.X, a.Y + b.Y);
+        public static Pos operator -(Pos a, Pos b) => new(a.X - b.X, a.Y - b.Y);
+
+        public static Pos LU = new(-1, 1);
+        public static Pos RU = new(1, 1);
+        public static Pos LD = new(-1, -1);
+        public static Pos RD = new(1, -1);
+        public static Pos Zero = new();
     };
 
+    private readonly Dictionary<string, Pos> _posMap = new()
+    {
+        {"L", new Pos(-1, 0)},
+        {"R", new Pos(1, 0)},
+        {"D", new Pos(0, -1)},
+        {"U", new Pos(0, 1)},
+    };
 
     [ExpectedResult(6498, Sample = 13)]
     public object SolvePartOne(string input)
@@ -38,42 +51,41 @@ public class Y2022D09 : ISolution
     private int GetUniquePositions((string, int)[] headMovements, int numPoints, int trackPointIdx)
     {
         HashSet<Pos> uniquePos = new();
-
         var points = new Pos[numPoints];
 
         foreach ((string dir, int dist) in headMovements)
         {
             for (int step = 0; step < dist; step++)
             {
-                points[0] += dir switch
-                {
-                    "L" => new Pos(-1, 0),
-                    "R" => new Pos(1, 0),
-                    "D" => new Pos(0, -1),
-                    "U" => new Pos(0, 1),
-                };
+                points[0] += _posMap[dir];
 
                 for (int pi = 1; pi < points.Length; pi++)
                 {
-                    var h = points[pi - 1];
-                    var t = points[pi];
-                    var delta = h - t;
-                    points[pi] += delta switch
+                    var delta = points[pi - 1] - points[pi];
+                    var mv = delta switch
                     {
-                        {X: > 1, Y: > 1} => delta + new Pos(-1, -1),
-                        {X: < -1, Y: < -1} => delta + new Pos(1, 1),
-                        {X: < -1, Y: > 1} => delta + new Pos(1, -1),
-                        {X: > 1, Y: < -1} => delta + new Pos(-1, 1),
+                        {X: > 1, Y: > 1} => delta + Pos.LD,
+                        {X: < -1, Y: < -1} => delta + Pos.RU,
+                        {X: < -1, Y: > 1} => delta + Pos.RD,
+                        {X: > 1, Y: < -1} => delta + Pos.LU,
                         {X: > 1} => delta with {X = 1},
                         {X: < -1} => delta with {X = -1},
                         {Y: > 1} => delta with {Y = 1},
                         {Y: < -1} => delta with {Y = -1},
-                        _ => new Pos()
+                        _ => Pos.Zero
                     };
 
+                    points[pi] += mv;
+                    
                     if (pi == trackPointIdx)
                     {
                         uniquePos.Add(points[pi]);
+                    }
+                    
+                    // If this point didn't move, no point after it will move either.
+                    if (mv.X == 0 && mv.Y == 0)
+                    {
+                        break;
                     }
                 }
             }
