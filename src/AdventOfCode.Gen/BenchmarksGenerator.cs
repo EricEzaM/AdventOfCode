30deef39456173565da7ed536224ee9de373f21b
Eric M <itsjusteza@gmail.com>
2022-12-09 23:37:46 +1000

    Add 'bench' command for benchmarking with BenchmarkDotnet, benchmark classes generated using source generator.

---
+++ src/AdventOfCode.Gen/BenchmarksGenerator.cs
@@ -0,0 +1,71 @@
+﻿using System.Diagnostics;
+using System.Text;
+using System.Text.RegularExpressions;
+using Microsoft.CodeAnalysis;
+
+namespace AdventOfCode.Gen;
+
+[Generator]
+public class BenchmarksGenerator : ISourceGenerator
+{
+    public void Initialize(GeneratorInitializationContext context)
+    {
+// #if DEBUG
+//         if (!Debugger.IsAttached)
+//         {
+//             Debugger.Launch();
+//         }
+// #endif 
+    }
+
+    public void Execute(GeneratorExecutionContext context)
+    {
+        var solutionFiles = context.Compilation.SyntaxTrees
+                                   .Where(s => Regex.IsMatch(s.FilePath, @"Y\d{4}D\d{2}(A\d+)?\.cs"))
+                                   .Select(s => s.FilePath)
+                                   .ToList();
+        
+        foreach (var file in solutionFiles)
+        {
+            var dir = Path.GetDirectoryName(file);
+            string input = File.ReadAllText(Path.Combine(dir, "input.txt"), Encoding.UTF8);
+            string className = Path.GetFileName(file).Replace(".cs", "");
+
+            var match = Regex.Match(className, @"Y(\d{4})D(\d{2})");
+            
+            string source = $@"// auto-generated
+using AdventOfCode.Y{match.Groups[1]}.D{match.Groups[2]};
+using AdventOfCode.Lib;
+using BenchmarkDotNet.Attributes;
+
+namespace AdventOfCode.Benchmarks;
+
+public class {className}Benchmark
+{{
+private string _input = @""{input}"";
+private ISolution _solution;
+
+[GlobalSetup]
+public void GlobalSetup()
+{{
+    _solution = new {className}();
+}}
+
+[Benchmark]
+public void PartOne()
+{{
+    _solution.SolvePartOne(_input);
+}}
+
+[Benchmark]
+public void PartTwo()
+{{
+    _solution.SolvePartTwo(_input);
+}}
+}}
+";
+            context.AddSource($"{className}Benchmark.g.cs", source);
+            
+        }
+    }
+}
---
+++ src/AdventOfCode.Lib/Cli/BenchmarkCommand.cs
@@ -0,0 +1,40 @@
+﻿using System.CommandLine;
+using System.Reflection;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Filters;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+
+namespace AdventOfCode.Lib.Cli;
+
+public class BenchmarkCommand : Command
+{
+    public BenchmarkCommand() : base("bench", "Run a benchmark on a puzzle.")
+    {
+        var yearArg = new Argument<int>("year", "The year filter.");
+        var dayArg = new Argument<int>("day", "The day filter.");
+        
+        var quickOpt = new Option<bool>(new []{"--quick", "-q"}, "Use quick benchmark configuration.");
+        
+        Add(yearArg);
+        Add(dayArg);
+        Add(quickOpt);
+        
+        this.SetHandler(HandleCommand, yearArg, dayArg, quickOpt);
+    }
+
+    private void HandleCommand(int year, int day, bool quick)
+    {
+        Console.WriteLine($"Running benchmark for Y={year} D={day}");
+
+        var config = ManualConfig
+                     .CreateMinimumViable()
+                     .AddJob(quick ? Job.ShortRun : Job.Default)
+                     .AddDiagnoser(new MemoryDiagnoser(new MemoryDiagnoserConfig(false)))
+                     .AddFilter(new SimpleFilter(c => c.Descriptor.Type.Name.Contains($"Y{year}D{day:00}")));
+
+        BenchmarkSwitcher.FromAssembly(Assembly.GetEntryAssembly())
+                         .RunAllJoined(config);
+    }
+}
